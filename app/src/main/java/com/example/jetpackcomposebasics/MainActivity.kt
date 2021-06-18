e87cf31228c949d95891d42ac42f681a9ca35d9d
cd-glacier <hyoga0216@gmail.com>
2021-06-18 13:46:02 +0900

    Make Counter pure function

--- app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -40,13 +40,18 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Composable
 fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    val counterState = remember { mutableStateOf(0) }
+
     Column {
         for (name in names) {
             Greeting(name = name)
             Divider(color = Color.Black)
         }
         Divider(color = Color.Transparent, thickness = 32.dp)
-        Counter()
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount -> counterState.value = newCount }
+        )
     }
 }
 
@@ -56,12 +61,9 @@ fun Greeting(name: String) {
 }
 
 @Composable
-fun Counter() {
-
-    val count = remember { mutableStateOf(0) }
-
-    Button(onClick = { count.value++ }) {
-        Text("I've been clicked ${count.value} times")
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(onClick = { updateCount(count + 1) }) {
+        Text("I've been clicked $count times")
     }
 }
 
